824c89bde33c52bdd3404aa8ae6c56e146904041
eleanor keane <egjkeane@gmail.com>
2017-06-08 11:39:37 +0100

    buildBoard() changes

--- src/js/dom-scripts.js
+++ src/js/dom-scripts.js
@@ -1,13 +1,13 @@
 let then, fpsInterval;
 const { crunchSpriteState,crunchState,checkIfWall,makeBoardPiece,isEdge} = require('./logic.js');
 let {currentState, lastKeyPressed} = require('./logic.js');
-const boardContainer = document.getElementById('board-container');
 const pacmanSprite = document.getElementById('pacman-sprite');
 
 function startAnimating(fps) {
   fpsInterval = 1000 / fps;
   then = Date.now();
   startTime = then;
+  buildBoard(currentState.board.size, currentState.board.rows, currentState.board.size/currentState.board.rows);
   mainLoop();
 }
 
@@ -15,38 +15,44 @@ const tick = {
   type: 'Tick',
   deltaInMilliseconds: 10000,
   input: 'up'
-}
+};
 
-boardContainer.setAttribute('style', `min-width: ${currentState.board.rows*currentState.board.cellSize+currentState.board.cellSize}em; max-width: ${currentState.board.rows*currentState.board.cellSize+currentState.board.cellSize}em;`);
-
-const buildBoard = function(size, rows, cols) {
-  for (let i = 1; i <= rows; i++ ) {
-    for (let j = 1; j <= cols; j++ ) {
-      let newDiv = document.createElement('div');
-      newDiv.classList = "board-tile";
-      newDiv.id=`${i}x${j}`;
-      // newDiv.textContent = `row ${i}/ col ${j}`;
-      if ( i === 1 || i === rows || j === 1 || j === cols ) {
-        if (i !== 7 && i !== 8 && i !== 6 && i !== 5 && j!==5 && j !== 7 && j !== 8 && j !== 6) {
-          newDiv = makeBoardPiece(newDiv, false, currentState.board.cellSize, newDiv.id);
+function buildTheBoard(boardContainer) {
+  console.log("SHuT up");
+  boardContainer.setAttribute('style', `min-width: ${currentState.board.rows*currentState.board.cellSize+currentState.board.cellSize}em; max-width: ${currentState.board.rows*currentState.board.cellSize+currentState.board.cellSize}em;`);
+  return (size, rows, cols) => {
+    console.log("rebuilding");
+    for (let i = 1; i <= rows; i++ ) {
+      for (let j = 1; j <= cols; j++ ) {
+        let newDiv = document.createElement('div');
+        newDiv.classList = "board-tile";
+        newDiv.id=`${i}x${j}`;
+        // newDiv.textContent = `row ${i}/ col ${j}`;
+        if ( i === 1 || i === rows || j === 1 || j === cols ) {
+          if (i !== 7 && i !== 8 && i !== 6 && i !== 5 && j!==5 && j !== 7 && j !== 8 && j !== 6) {
+            newDiv = makeBoardPiece(newDiv, false, currentState.board.cellSize, newDiv.id);
+          } else {
+            newDiv = makeBoardPiece(newDiv, true, currentState.board.cellSize, newDiv.id);
+          }
         } else {
-          newDiv = makeBoardPiece(newDiv, true, currentState.board.cellSize, newDiv.id);
-        }
-      } else {
-          newDiv = makeBoardPiece(newDiv, true, currentState.board.cellSize, newDiv.id);
-        }
-      let boardArr = currentState.board.boardArr;
-      let boardObj = currentState.board.boardObj;
-      boardArr.push(newDiv);
-      boardObj[newDiv.id] = newDiv;
+            newDiv = makeBoardPiece(newDiv, true, currentState.board.cellSize, newDiv.id);
+          }
+        let boardArr = currentState.board.boardArr;
+        let boardObj = currentState.board.boardObj;
+        boardArr.push(newDiv);
+        boardObj[newDiv.id] = newDiv;
+      }
     }
-  }
-  for (let i = 0; i < currentState.board.boardArr.length; i++ ) {
-  boardContainer.append(currentState.board.boardArr[i].physicalEntity);
-  }
+    for (let i = 0; i < currentState.board.boardArr.length; i++ ) {
+    boardContainer.append(currentState.board.boardArr[i].physicalEntity);
+    }
+  };
 }
+
+const buildBoard = buildTheBoard(document.getElementById('board-container'));
 
 function renderBoard(state) {
+  buildBoard(currentState.board.size, currentState.board.rows, currentState.board.size/currentState.board.rows);
   affixSprite(pacmanSprite, state.pacman);
 }
 
@@ -79,5 +85,4 @@ document.addEventListener('keydown', (e)=> {
   }
 });
 
-buildBoard(currentState.board.size, currentState.board.rows, currentState.board.size/currentState.board.rows);
 startAnimating(2);
