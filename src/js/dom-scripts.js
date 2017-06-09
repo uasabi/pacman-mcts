1c4f797b05edfb004d94c760954499c6f3f047c4
Daniele Polencic <daniele.polencic@gmail.com>
2017-06-09 14:32:56 +0100

    refactored rendering

--- src/js/dom-scripts.js
+++ src/js/dom-scripts.js
@@ -1,60 +1,36 @@
 let then, fpsInterval, startTime;
-const {crunchState, makeBoardPiece} = require('./logic');
-let {currentState, lastKeyPressed} = require('./logic');
-const pacmanSprite = document.getElementById('pacman-sprite');
-const redSprite = document.getElementById('ghost-sprite-r');
-const orangeSprite = document.getElementById('ghost-sprite-o');
+const {crunchState, makeBoardPiece, makeOrange, makePacman, makeRed} = require('./pre-logic');
+let {currentState, lastKeyPressed} = require('./pre-logic');
 
 function startAnimating(fps) {
   fpsInterval = 1000 / fps;
   then = Date.now();
   startTime = then;
-  buildBoard(currentState.board.size, currentState.board.rows, currentState.board.size/currentState.board.rows);
   mainLoop();
 }
 
-function buildTheBoard(boardContainer) {
-  boardContainer.setAttribute('style', `min-width: ${currentState.board.rows*currentState.board.cellSize+currentState.board.cellSize}em; max-width: ${currentState.board.rows*currentState.board.cellSize+currentState.board.cellSize}em;`);
-  return (size, rows, cols) => {
-    for (let i = 1; i <= rows; i++ ) {
-      for (let j = 1; j <= cols; j++ ) {
-        let newDiv = document.createElement('div');
-        newDiv.classList = 'board-tile';
-        newDiv.id=`${i}x${j}`;
-        // newDiv.textContent = `row ${i}/ col ${j}`;
-        if ( i === 1 || i === rows || j === 1 || j === cols ) {
-          if (i !== 7 && i !== 8 && i !== 6 && i !== 5 && j!==5 && j !== 7 && j !== 8 && j !== 6) {
-            newDiv = makeBoardPiece(newDiv, false, currentState.board.cellSize, newDiv.id);
-          } else {
-            newDiv = makeBoardPiece(newDiv, true, currentState.board.cellSize, newDiv.id);
-          }
-        } else {
-          newDiv = makeBoardPiece(newDiv, true, currentState.board.cellSize, newDiv.id);
-        }
-        let boardArr = currentState.board.boardArr;
-        let boardObj = currentState.board.boardObj;
-        boardArr.push(newDiv);
-        boardObj[newDiv.id] = newDiv;
-      }
-    }
-    for (let i = 0; i < currentState.board.boardArr.length; i++ ) {
-      boardContainer.append(currentState.board.boardArr[i].physicalEntity);
-    }
-  };
-}
+function buildTheBoard(size, rows, cols) {
+  const makeColumnForRow = (rowIndex) => Array(cols).fill(0)
+    .map((_, columnIndex) => makeBoardPiece(`${rowIndex}x${columnIndex}`, currentState.board.cellSize));
+  const matrix = Array(rows).fill(0).map((_, rowIndex) => makeColumnForRow(rowIndex));
 
-const buildBoard = buildTheBoard(document.getElementById('board-container'));
+  currentState.board.walls.forEach(wall => {
+    matrix[wall.y][wall.x] = makeBoardPiece(`${wall.y}x${wall.x}`, currentState.board.cellSize, false);
+  });
+
+  matrix[currentState.pacman.y][currentState.pacman.x] = makePacman(currentState.board.cellSize);
+  matrix[currentState.red.y][currentState.red.x] = makeRed(currentState.board.cellSize);
+  matrix[currentState.orange.y][currentState.orange.x] = makeOrange(currentState.board.cellSize);
+
+  return `
+    <div style="max-width: ${(currentState.board.rows * currentState.board.cellSize) + 7}em;">
+      ${matrix.map(it => it.join('')).join('')}
+    </div>`;
+}
 
 function renderBoard(state) {
-  buildBoard();
-  affixSprite(pacmanSprite, state.pacman);
-  affixSprite(redSprite, state.red);
-  affixSprite(orangeSprite, state.orange);
-}
-
-function affixSprite(sprite, spriteState) {
-  let nextSpritePlace = document.getElementById(`${spriteState.y}x${spriteState.x}`);
-  nextSpritePlace.append(sprite);
+  const boardHtml = buildTheBoard(state.board.size, state.board.rows, state.board.rows);
+  document.getElementById('board-container').innerHTML = boardHtml;
 }
 
 function mainLoop() {
@@ -63,7 +39,7 @@ function mainLoop() {
   let elapsed = now - then;
   if (elapsed > fpsInterval) {
     then = now - (elapsed % fpsInterval);
-    currentState =  crunchState(currentState, {type: 'Tick', deltaInMilliseconds: then, input: lastKeyPressed}) || currentState;
+    currentState = crunchState(currentState, {type: 'Tick', deltaInMilliseconds: then, input: lastKeyPressed}) || currentState;
     renderBoard(currentState);
   }
 }
