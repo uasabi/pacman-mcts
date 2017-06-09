5a9bf6ec1db993d404df4aef9cdef4a9f1f44f33
eleanor keane <egjkeane@gmail.com>
2017-06-09 12:09:59 +0100

    changes to buildboard

--- src/js/dom-scripts.js
+++ src/js/dom-scripts.js
@@ -9,44 +9,44 @@ function startAnimating(fps) {
   fpsInterval = 1000 / fps;
   then = Date.now();
   startTime = then;
-  buildBoard(currentState.board.size, currentState.board.rows, currentState.board.size/currentState.board.rows);
+  buildBoard(document.getElementById('board-container'), currentState);
   mainLoop();
 }
 
-function buildTheBoard(boardContainer) {
+function buildBoard(boardContainer, state) {
   boardContainer.setAttribute('style', `min-width: ${currentState.board.rows*currentState.board.cellSize+currentState.board.cellSize}em; max-width: ${currentState.board.rows*currentState.board.cellSize+currentState.board.cellSize}em;`);
-  return (size, rows, cols) => {
-    for (let i = 1; i <= rows; i++ ) {
-      for (let j = 1; j <= cols; j++ ) {
-        let newDiv = document.createElement('div');
-        newDiv.classList = 'board-tile';
-        newDiv.id=`${i}x${j}`;
-        // newDiv.textContent = `row ${i}/ col ${j}`;
-        if ( i === 1 || i === rows || j === 1 || j === cols ) {
-          if (i !== 7 && i !== 8 && i !== 6 && i !== 5 && j!==5 && j !== 7 && j !== 8 && j !== 6) {
-            newDiv = makeBoardPiece(newDiv, false, currentState.board.cellSize, newDiv.id);
-          } else {
-            newDiv = makeBoardPiece(newDiv, true, currentState.board.cellSize, newDiv.id);
-          }
+  const board = state.board;
+  const {rows, size} = board;
+  const cols = size/rows;
+  for (let i = 1; i <= rows; i++ ) {
+    for (let j = 1; j <= cols; j++ ) {
+      let newDiv = document.createElement('div');
+      newDiv.classList = 'board-tile';
+      newDiv.id=`${i}x${j}`;
+      // newDiv.textContent = `row ${i}/ col ${j}`;
+      if ( i === 1 || i === rows || j === 1 || j === cols ) {
+        if (i !== 7 && i !== 8 && i !== 6 && i !== 5 && j!==5 && j !== 7 && j !== 8 && j !== 6) {
+          newDiv = makeBoardPiece(newDiv, false, currentState.board.cellSize, newDiv.id);
         } else {
           newDiv = makeBoardPiece(newDiv, true, currentState.board.cellSize, newDiv.id);
         }
-        let boardArr = currentState.board.boardArr;
-        let boardObj = currentState.board.boardObj;
-        boardArr.push(newDiv);
-        boardObj[newDiv.id] = newDiv;
+      } else {
+        newDiv = makeBoardPiece(newDiv, true, currentState.board.cellSize, newDiv.id);
       }
+      let boardArr = currentState.board.boardArr;
+      let boardObj = currentState.board.boardObj;
+      boardArr.push(newDiv);
+      boardObj[newDiv.id] = newDiv;
     }
-    for (let i = 0; i < currentState.board.boardArr.length; i++ ) {
-      boardContainer.append(currentState.board.boardArr[i].physicalEntity);
-    }
-  };
+  }
+  boardContainer.innerText = "";
+  for (let i = 0; i < currentState.board.boardArr.length; i++ ) {
+    boardContainer.append(currentState.board.boardArr[i].physicalEntity);
+  }
 }
 
-const buildBoard = buildTheBoard(document.getElementById('board-container'));
-
 function renderBoard(state) {
-  buildBoard();
+  buildBoard(document.getElementById('board-container'), currentState);
   affixSprite(pacmanSprite, state.pacman);
   affixSprite(redSprite, state.red);
   affixSprite(orangeSprite, state.orange);
