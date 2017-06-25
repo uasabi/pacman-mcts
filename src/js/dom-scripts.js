0f2f42b2f27639c46960669cb0116e594bfb0648
Daniele Polencic <daniele.polencic@gmail.com>
2017-06-25 09:43:31 +0100

    removed sharing by reference for state and lastKeyPress

--- src/js/dom-scripts.js
+++ src/js/dom-scripts.js
@@ -1,9 +1,65 @@
 import {renderBoard} from './render';
+import {crunchState, NONE, LEFT, RIGHT, UP, DOWN} from './pre-logic';
 
 let then, fpsInterval, startTime;
-let count = 0;
-const {crunchState, makeBoardPiece, makeOrange, makePacman, makeRed} = require('./pre-logic');
-let {currentState, lastKeyPressed} = require('./pre-logic');
+let lastKeyPressed = NONE;
+let currentState = {
+  collision: false,
+  board: {
+    size: 144,
+    rows: 12,
+    cellSize: 4,
+    walls: [
+      {x: 0, y: 0},
+      {x: 1, y: 0},
+      {x: 2, y: 0},
+      {x: 3, y: 0},
+      {x: 8, y: 0},
+      {x: 9, y: 0},
+      {x: 10, y: 0},
+      {x: 11, y: 0},
+      {y: 0, x: 0},
+      {y: 1, x: 0},
+      {y: 2, x: 0},
+      {y: 3, x: 0},
+      {y: 8, x: 0},
+      {y: 9, x: 0},
+      {y: 10, x: 0},
+      {y: 11, x: 0},
+      {x: 0, y: 11},
+      {x: 1, y: 11},
+      {x: 2, y: 11},
+      {x: 3, y: 11},
+      {x: 8, y: 11},
+      {x: 9, y: 11},
+      {x: 10, y: 11},
+      {x: 11, y: 11},
+      {y: 0, x: 11},
+      {y: 1, x: 11},
+      {y: 2, x: 11},
+      {y: 3, x: 11},
+      {y: 8, x: 11},
+      {y: 9, x: 11},
+      {y: 10, x: 11},
+      {y: 11, x: 11}
+    ]
+  },
+  pacman: {
+    x: 5,
+    y: 5,
+    direction: LEFT,
+  },
+  red: {
+    x: 7,
+    y: 11,
+    direction: RIGHT
+  },
+  orange: {
+    x: 1,
+    y: 8,
+    direction: UP
+  }
+};
 
 function startAnimating(fps) {
   fpsInterval = 1000 / fps;
@@ -20,8 +76,8 @@ function mainLoop() {
     then = now - (elapsed % fpsInterval);
     currentState = crunchState(currentState, {type: 'Tick', deltaInMilliseconds: then, input: {
       pacman: lastKeyPressed,
-      red: 'nope',
-      orange: 'nope'
+      red: NONE,
+      orange: NONE
     }}) || currentState;
     renderBoard(currentState);
   }
@@ -31,18 +87,18 @@ document.addEventListener('keydown', (e)=> {
   e = e || window.event;
   switch(e.keyCode) {
   case 37:
-    lastKeyPressed = 'left';
+    lastKeyPressed = LEFT;
     break;
   case 38:
-    lastKeyPressed = 'up';
+    lastKeyPressed = UP;
     break;
   case 39:
-    lastKeyPressed = 'right';
+    lastKeyPressed = RIGHT;
     break;
   case 40:
-    lastKeyPressed = 'down';
+    lastKeyPressed = DOWN;
     break;
   }
 });
 
-startAnimating(0.4);
+startAnimating(1);
--- src/js/pre-logic.js
+++ src/js/pre-logic.js
@@ -1,69 +1,8 @@
-export let lastKeyPressed = 'nope';
-
 export const LEFT = 'left';
 export const RIGHT = 'right';
 export const UP = 'up';
 export const DOWN = 'down';
 export const NONE = 'nope';
-
-export let currentState = {
-  collision: false,
-  board: {
-    size: 144,
-    rows: 12,
-    cellSize: 4,
-    walls: [
-      {x: 0, y: 0},
-      {x: 1, y: 0},
-      {x: 2, y: 0},
-      {x: 3, y: 0},
-      {x: 8, y: 0},
-      {x: 9, y: 0},
-      {x: 10, y: 0},
-      {x: 11, y: 0},
-      {y: 0, x: 0},
-      {y: 1, x: 0},
-      {y: 2, x: 0},
-      {y: 3, x: 0},
-      {y: 8, x: 0},
-      {y: 9, x: 0},
-      {y: 10, x: 0},
-      {y: 11, x: 0},
-      {x: 0, y: 11},
-      {x: 1, y: 11},
-      {x: 2, y: 11},
-      {x: 3, y: 11},
-      {x: 8, y: 11},
-      {x: 9, y: 11},
-      {x: 10, y: 11},
-      {x: 11, y: 11},
-      {y: 0, x: 11},
-      {y: 1, x: 11},
-      {y: 2, x: 11},
-      {y: 3, x: 11},
-      {y: 8, x: 11},
-      {y: 9, x: 11},
-      {y: 10, x: 11},
-      {y: 11, x: 11}
-    ]
-  },
-  pacman: {
-    x: 5,
-    y: 5,
-    direction: 'nope',
-    activeDirection: lastKeyPressed
-  },
-  red: {
-    x: 7,
-    y: 11,
-    direction: 'right'
-  },
-  orange: {
-    x: 1,
-    y: 8,
-    direction: 'up'
-  }
-};
 
 export function isWall({walls, x, y}) {
   return !!walls.find(element => element.x === x && element.y === y);
@@ -72,7 +11,6 @@ export function isWall({walls, x, y}) {
 export function crunchState(state, action) {
   if (state.collision) return state;
 
-  currentState.pacman.activeDirection = action.input;
   const pacManInput = action.input.pacman === NONE ? state.pacman.direction : action.input.pacman;
   const redInput = action.input.red === NONE ? state.red.direction : action.input.red;
   const orangeInput = action.input.orange === NONE ? state.orange.direction : action.input.orange;
